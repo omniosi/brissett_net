f676dccdaa0b965b963953bb34c050180cfc8033
omniosi <omniosi@gmail.com>
2024-12-01 22:15:29 -0500

    Launch QA

--- code/static/js/script.js
+++ code/static/js/script.js
@@ -12,19 +12,3 @@ menu.addEventListener("click", (event) => {
     slideout.classList.add("open");
   }
 })
-
-document.getElementById("contact-form").addEventListener("submit", function(event)  {
-  event.preventDefault(); // Prevent default form submission
-
-  // Get form data
-  var name = document.getElementById("name").value;
-  var email = document.getElementById("email").value;
-  var message = document.getElementById("message").value;
-
-  // Create email subject and body
-  var subject = "New Contact Form Submission";
-  var body = "- Name: " + name + "\n- Email: " + email + "\n- Message: " + message;
-
-  // Create a new email window
-  window.location.href = "mailto:info@brissett.net?subject=" + subject + "&body=" + body;
-});
